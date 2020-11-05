eb140166c1d7143f5261279100c51278c6f300ab
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-05 14:45:49 -0600

    adds even constants

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'react-calendar/dist/Calendar.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.querySelector('#root')
-);
+import { Provider } from 'react-redux';
+import { configureStore, history } from './app/store/configureStore';
+import ScrollToTop from './app/layout/ScrollToTop';
+import { ConnectedRouter } from 'connected-react-router';
+
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+function render() {
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <ScrollToTop />
+        <App />
+      </ConnectedRouter>
+    </Provider>,
+    rootEl
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('./app/layout/App', function () {
+    setTimeout(render);
+  });
+}
+
+render();
