3a690221b313ef5c2e27983f424ed4acd7839071
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-05 10:00:49 -0600

    modifies form to send setFormOpen through props

--- src/features/nav/NavBar.jsx
+++ src/features/nav/NavBar.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Button, Container, Menu } from 'semantic-ui-react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+import SignedOutMenu from './SignedOutMenu';
+import SignedInMenu from './SignedInMenu';
+import { useSelector } from 'react-redux';
 
-export default function NavBar() {
+export default function NavBar({ setFormOpen }) {
+  const { authenticated } = useSelector((state) => state.auth);
+
   return (
     <Menu inverted fixed='top'>
       <Container>
-        <Menu.Item header>
+        <Menu.Item as={NavLink} exact to='/' header>
           <img src='/assets/logo.png' alt='logo' style={{ marginRight: 15 }} />
           Re-vents
         </Menu.Item>
-        <Menu.Item name='Events' />
-        <Button positive inverted content='Create Event' />
-        <Menu.Item position='right'>
-          <Button inverted content='Login' />
-          <Button inverted content='Register' style={{ marginLeft: '0.5em' }} />
-        </Menu.Item>
+        <Menu.Item as={NavLink} to='/events' name='Events' />
+        <Menu.Item as={NavLink} to='/sandbox' name='Sandbox' />
+        {authenticated && (
+          <Menu.Item as={NavLink} to='/createEvent'>
+            <Button positive inverted content='Create Event' />
+          </Menu.Item>
+        )}
+        {authenticated ? <SignedInMenu /> : <SignedOutMenu />}
       </Container>
     </Menu>
   );
